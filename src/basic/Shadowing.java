15e7579e92308a0c6250d35e8deaebf00bf2bc38
JAVA-DSA <ronbonnke789@gmail.com>
2024-02-07 18:44:03 +0530

    practise

--- src/basic/Shadowing.java
+++ src/basic/Shadowing.java
@@ -1,7 +1,14 @@
 package basic;
 
 public class Shadowing {
-    public static void main(String[] args) {
-
+    static int x = 50;  //initializing class variable:  line 7 is shadowed here
+         public static void main(String[] args) {
+             System.out.println(x); // 10
+             int x = 90; //initializing local variable:   class variable at line 4 is shadowed here
+             System.out.println(x); // 90
+            fun();
+         }
+         static void fun(){
+        System.out.println(x); // print 10  because it is outside the method/function
     }
 }
