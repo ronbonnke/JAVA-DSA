dfe9990ce5dd3f91b37f0afc8bd117be760f2d1a
JAVA-DSA <ronbonnke789@gmail.com>
2024-02-08 22:41:19 +0530

    practise

--- src/basic/Questions.java
+++ src/basic/Questions.java
@@ -4,10 +4,28 @@ import java.util.Scanner;
 
 public class Questions {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        System.out.println(isPrime(n));
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        System.out.println(isPrime(n));
+//        System.out.println(isArmstrong(n));
+        for (int i = 100; i < 1000; i++) {
+            if (isArmstrong(i)){
+                System.out.println(i);
+            }
+        }
     }
+    static boolean isArmstrong(int n){
+         int org = n;
+         int sum = 0;
+         while (n > 0){
+             int rem = n % 10;
+             n = n / 10;
+             sum = sum + rem*rem*rem;
+         }
+         return sum == org;
+    }
+
+
     static boolean isPrime(int n){
         if(n<=1){
             return false;
