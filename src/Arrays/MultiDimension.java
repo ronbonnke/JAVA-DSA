b96dedcdd9e8f691f09a6507142436475ee48082
JAVA-DSA <ronbonnke789@gmail.com>
2024-02-12 10:14:10 +0530

    practise

--- src/Arrays/MultiDimension.java
+++ src/Arrays/MultiDimension.java
@@ -1,4 +1,12 @@
 package Arrays;
 
 public class MultiDimension {
+    public static void main(String[] args) {
+        int[][] arr2D = {
+                {1, 2, 3},
+                {4, 5},
+                {6, 7, 8, 9}
+        };
+        System.out.println(arr2D[2][3]);
+    }
 }
