982cba90c11beea14761717fef18834e194624f4
JAVA-DSA <ronbonnke789@gmail.com>
2024-02-16 17:32:34 +0530

    practise

--- src/Objects/InnerClasses.java
+++ src/Objects/InnerClasses.java
@@ -1,9 +1,23 @@
 package Objects;
 
  public class InnerClasses {
+
     static class Test {
+         String name;
+         public Test(String name){
+             this.name = name;
+         }
 
     }
+
+     public static void main(String[] args) {
+        Test a = new Test("ron");
+        Test b = new Test("bonnke");
+
+         System.out.println(a.name);
+         System.out.println(b.name);
+     }
+
 }
 
 // outside classes cannot be static
