8709cdb14f3277fb9eb8ae0d4684735292d62b1f
JAVA-DSA <ronbonnke789@gmail.com>
2024-02-18 21:52:33 +0530

    practise

--- src/Singleton/Singleton.java
+++ src/Singleton/Singleton.java
@@ -3,10 +3,13 @@ package Singleton;
 public class Singleton {
     int num = 0;
 private static Singleton instance;
+
     public static Singleton getInstance() {
+
         if (instance == null){
             instance = new Singleton();
         }
         return instance;
     }
+
 }
