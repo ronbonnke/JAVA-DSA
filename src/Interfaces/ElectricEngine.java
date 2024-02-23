accfcc6a61fcca624980c14ebc1e21975ad7fa17
JAVA-DSA <ronbonnke789@gmail.com>
2024-02-23 20:02:52 +0530

    practise

---
+++ src/Interfaces/ElectricEngine.java
@@ -0,0 +1,23 @@
+package Interfaces;
+
+public class ElectricEngine implements Engine{
+
+
+    @Override
+    public void stop() {
+        System.out.println("Electric engine stop");
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Electric engine starts");
+    }
+    @Override
+    public void acc() {
+        System.out.println("Electric engine accelerate");
+    }
+
+
+}
+
+
