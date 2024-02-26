fab05109c711d5eae228c0d18bf22926738008af
JAVA-DSA <ronbonnke789@gmail.com>
2024-02-26 13:07:15 +0530

    practise

---
+++ src/cloning/Main.java
@@ -0,0 +1,18 @@
+package cloning;
+
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        Human ron = new Human(20,"bonnke");
+
+        Human twin = (Human)ron.clone();
+
+        System.out.println(Arrays.toString(twin.arr));
+        System.out.println(twin.name);
+
+        twin.arr[0] = 100;
+        System.out.println(Arrays.toString(twin.arr));
+        System.out.println(Arrays.toString(ron.arr));
+    }
+}
--- src/exceptionHandling/Main.java
+++ src/exceptionHandling/Main.java
@@ -1,23 +1,33 @@
 package exceptionHandling;
 
 public class Main {
+
     public static void main(String[] args) {
         int a = 10;
         int b = 0;
 
         try{
-            divide(a,b);
+            //divide(a,b);
+
+
+            //mimicing
+            String name = "bonnke";
+            if (name.equals("bonnke")){
+                throw new MyExceptions("name is bonnke");
+            }
+        }
+        catch(MyExceptions e) {
+            System.out.println(e.getMessage());
         }
         catch(ArithmeticException e) {
             System.out.println(e.getMessage());
         }
         catch(Exception e){
             System.out.println("Normal exception");
-
         }
         finally
         {
-            System.out.println("it will execute");
+            System.out.println("it will always execute");
         }
     }
     static int divide(int a, int b) throws Exception {
