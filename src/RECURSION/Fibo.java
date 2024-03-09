f2579b31c1c5264170a421c1d07e58af30dd2c4b
JAVA-DSA <ronbonnke789@gmail.com>
2024-03-09 12:37:38 +0530

    practise

--- src/RECURSION/Fibo.java
+++ src/RECURSION/Fibo.java
@@ -2,12 +2,12 @@ package RECURSION;
 
 public class Fibo {
     public static void main(String[] args) {
-        System.out.println(fibo(40));
+        System.out.println(fibo(4));
     }
     static int fibo(int n){
         if(n < 2){
             return n;
         }
-        return  fibo(n-1)+ fibo(n-2);
+        return  fibo(n-1)+ fibo(n-20);
     }
 }
