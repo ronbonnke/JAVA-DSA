65c6d363d7ce7ae2840b243796e0341e35bfc5c1
JAVA-DSA <ronbonnke789@gmail.com>
2024-03-09 13:03:36 +0530

    practise

--- src/RECURSION/BinarySearch.java
+++ src/RECURSION/BinarySearch.java
@@ -2,7 +2,7 @@ package RECURSION;
 
 public class BinarySearch {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 55, 66, 78};
+        int[ ] arr = {1, 2, 3, 4, 55, 66, 78};
         int target = 4;
         System.out.println(search(arr, target, 0, arr.length-1));
 
